46fdd345c58581837122b73bc8faa890e0b1aa3c
Kat Russo <acrimonster@gmail.com>
2017-09-06 09:10:29 -0400

    Add routing to app

---
+++ todos/src/components/SomeOtherComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SomeOtherComponent extends React.Component {
+  render() {
+    return (
+      <h2>Some stuff</h2>
+    )
+  }
+}
+
+export default SomeOtherComponent
--- todos/src/index.js
+++ todos/src/index.js
@@ -3,4 +3,31 @@ import {render} from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 
-render(<App />, document.getElementById('main'));
+//use for redirecting/routing to different components based on path
+//history is a requirement of Router
+//Switch replaced "Miss" and redirects when original route is not found
+import NotFound from './components/NotFound';
+import SomeOtherComponent from './components/SomeOtherComponent';
+
+import {Router, Route, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const history = createBrowserHistory()
+
+const Root = () => {
+  return (
+  	<Router history={history}>
+ 		<div>
+		    <Switch>
+		    	<Route exact path='/' component={App} />
+				<Route path='/someOtherComponent' component={SomeOtherComponent} />
+	      		<Route component={NotFound} />
+		    </Switch>
+		</div>
+	</Router>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
+
+// render(<App />, document.getElementById('main'));
